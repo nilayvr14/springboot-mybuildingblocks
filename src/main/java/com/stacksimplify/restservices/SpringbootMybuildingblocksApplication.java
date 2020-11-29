f39ba4e1341b6136f995fb0408b4dba103c26993
Nilay Vora <nilayvr@gmail.com>
2020-11-29 23:57:56 +0530

    Trying to commit from IDE

--- src/main/java/com/stacksimplify/restservices/SpringbootMybuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootMybuildingblocksApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootMybuildingblocksApplication {
-
+//some comment to test commit from IDE
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMybuildingblocksApplication.class, args);
 	}
