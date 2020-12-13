6b31cce249c06e7b95c7c0defd93f427e5f72a0d
Nilay Vora <nilayvr@gmail.com>
2020-12-13 18:44:33 +0530

    JPA Base Rest Services Development Completed

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values (101, 'nilayvr@gmail.com', 'Nilay', 'Vora', 'admin', 'ssn101', 'nilayvr');
+insert into user values (102, 'ketanvora@gmail.com', 'Ketan', 'Vora', 'admin', 'ssn102', 'ketanvora66');
+insert into user values (103, 'mananshah@gmail.com', 'Manan', 'Shah', 'community', 'ssn103', 'mananshah23');
